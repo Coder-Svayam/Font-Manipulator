75e7cf6321f9fd92a233d90f476645ede1688f6f
Coder-Svayam <92960886+Coder-Svayam@users.noreply.github.com>
2022-11-01 20:58:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+noseX = 0;
+noseY = 0;
+
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
+function setup(){
+    var video = createCapture(VIDEO);
+    video.size(450, 400);
+
+    canvas = createCanvas(550, 450);
+    canvas.position(560, 75);
+
+}
+
+
+
+
+
